d604d9999dccdde9f2d995e291a13a9a975eb4ef
zhuliang <droideep@163.com>
2015-08-07 15:19:31 +0800

    remove build folder

--- kotlin/src/main/java/io/github/brightyoyo/MainActivity.kt
+++ kotlin/src/main/java/io/github/brightyoyo/MainActivity.kt
@@ -1,16 +1,16 @@
 package io.github.brightyoyo
 
-import android.support.v7.app.AppCompatActivity
+import android.support.v7.app.ActionBarActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 
-public class KotlinActivity : AppCompatActivity() {
+
+public class MainActivity : ActionBarActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_kotlin)
-
-
+        setContentView(R.layout.activity_main)
     }
+
 }
